b12765d18cb3deb719bc00a7082afbb44ef6c119
Jason Morrissette <JMorrissette@nkschools.org>
2016-11-01 09:08:58 -0700

    Bug Fixes
    Bug Fixes in Swiss Pairings

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,7 +1,7 @@
 -- deletes database if there is one in existance
-DROP DATABSE IF EXISTS tournament;
+DROP DATABASE IF EXISTS tournament;
 -- creates the tournament database
-CREATE DATABSE tournament;
+CREATE DATABASE tournament;
 -- connects to the tournament database
 \c tournament
 
@@ -15,8 +15,8 @@ CREATE TABLE players(
 -- creates Matches table
 CREATE TABLE matches(
     match_id SERIAL PRIMARY KEY,
-    winner INT REFRENCES players (player_id),
-    loser INT REFRENCES players (player_id)
+    winner INT REFERENCES players (player_id),
+    loser INT REFERENCES players (player_id)
 );
 
 -- count registered players view
@@ -26,20 +26,20 @@ CREATE OR REPLACE VIEW count_players AS
 
 -- create the view for player standings
 CREATE OR REPLACE VIEW current_standings AS
-    SELECT player_id,
-           plyaer_name.
-           SUM(CASE WHEN players.player_id = matches.winner THEN 1 ELSE 0 END) AS wins,
-           COUNT(matches) AS match_count
+    SELECT  player_id,
+            player_name,
+            SUM(CASE WHEN players.player_id = matches.winner THEN 1 ELSE 0 END) AS wins,
+            COUNT(matches) AS match_count
     FROM players
     LEFT OUTER JOIN matches
     ON players.player_id = matches.winner OR players.player_id = matches.loser
     GROUP BY player_id
     ORDER BY wins DESC,
              match_count ASC;
-
+             
 -- create a view to randomly seed the matches before the first round.
 CREATE OR REPLACE VIEW seed_initial_round AS
-    SEELCT * 
+    SELECT * 
     FROM players
     ORDER BY random();
 
